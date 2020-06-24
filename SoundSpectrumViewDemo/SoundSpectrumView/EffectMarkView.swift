d9608832e1baf137a063b1bca9b801d37cefb7c4
Modi <modi@cloudtrons.com>
2020-06-24 18:40:55 +0800

    @ 音效View

---
+++ SoundSpectrumViewDemo/SoundSpectrumView/EffectMarkView.swift
@@ -0,0 +1,15 @@
+//
+//  EffectMarkView.swift
+//  SoundSpectrumViewDemo
+//
+//  Created by Modi on 2020/6/24.
+//  Copyright © 2020 Modi. All rights reserved.
+//
+
+import UIKit
+
+class EffectMarkView: UIView {
+    
+    
+
+}
